0efeda8e33ce882266cd9204faf6655eaa7c5900
okumayuu <okumayuu1020@gmail.com>
2024-04-28 11:02:46 +0900

    8-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,12 @@ class Post extends Model
 
     
     use HasFactory;
+    
+    protected $fillable = [
+    'title',
+    'body',
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 3)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
